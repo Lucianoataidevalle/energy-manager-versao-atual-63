68b0d8c237cc2baaa0d21b2c731219d166806d65
hsga94 <hsgratao@gmail.com>
2024-12-11 14:23:24 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -18,4 +17,20 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          datepicker: ['react-day-picker', 'date-fns'],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['react-day-picker', 'date-fns'],
+    force: true
+  }
 }));
